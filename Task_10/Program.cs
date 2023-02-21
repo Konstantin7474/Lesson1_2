05a7c4902700c994ec4df37c9880fabbee3fdf00
konstantin <konstantin.duganov@mail.ru>
2023-02-21 17:09:04 +0800

    Task 10 commit

---
+++ Task_10/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+показывает вторую цифру этого числа.*/
+
+
+int N;
+Console.WriteLine($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+string str_N = N.ToString();
+
+if (str_N.Length > 3)
+        Console.Write($"В заданном числе больше цифр ");
+
+if (str_N.Length > 2)
+    Console.Write($"{N} -> {str_N[1]}");
+
+else
+    Console.Write($"Нет 2 цифры");
