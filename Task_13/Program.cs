ec6262c5b2f05a69957d4b2ebe7738bb801b791c
konstantin <konstantin.duganov@mail.ru>
2023-02-21 17:14:39 +0800

    Task 13 commit

---
+++ Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.*/
+
+
+int N;
+Console.WriteLine($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+string str_N = N.ToString();
+
+
+if (str_N.Length > 2)
+    Console.Write($"{N} -> {str_N[2]}");
+
+else
+    Console.Write($"Нет 3 цифры");
