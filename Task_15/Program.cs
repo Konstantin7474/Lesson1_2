6c81579d8874887699d8c465ebafd47f93aa4e9c
konstantin <konstantin.duganov@mail.ru>
2023-02-21 17:29:18 +0800

    Task 15 commit

---
+++ Task_15/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+
+int N;
+Console.WriteLine($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+ 
+ if (N > 7 || N < 1)
+    Console.Write($"Вы ввели неверное число ");
+if (N == 6 || N == 7)
+    Console.Write("Да");
+if (N == 5 || N == 4)
+    Console.WriteLine("Нет");
+if (N == 3 || N == 2)
+    Console.WriteLine("Нет");
+if (N == 1)
+    Console.WriteLine("Нет");
+
+    
+
