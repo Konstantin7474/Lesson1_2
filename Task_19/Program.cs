c587244817ae7683aba915e8d42e68d346efc24f
konstantin <konstantin.duganov@mail.ru>
2023-02-24 17:36:50 +0800

    Task 19 commit

---
+++ Task_19/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+ 
+int  GN;
+int N = GN;
+int P = 0;
+Console.WriteLine($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out GN);
+
+string str_N = N.ToString();
+
+if (str_N.Length > 5 || str_N.Length < 5)
+    Console.Write($"Вы ввели не пятизначное число ");
+
+while(N > 0)
+{
+    int NUM = N % 10;
+    P = (P * 10) + NUM;
+    N = N / 10;
+
+}
+if(P == GN)
+{
+    Console.Write($"Полиндром ");
+}
+else
+{
+    Console.Write($"Не полиндром ");
+}
+    
