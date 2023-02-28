27e3de976c6079ab31a0b75ea46b0f7c358b2144
konstantin <konstantin.duganov@mail.ru>
2023-02-28 18:32:54 +0800

    task 29 commit

---
+++ Task_29/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из
+ 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+
+/*int [] array = FillArray(8);
+
+
+int[] FillArray(int size, int min = 0, int max = 8)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min, max + 1);
+    return arr;
+}
+
+Console.Write(array);*/
+
+
+
+class ArrayDemo {
+    static void Main() {
+        int[] sample = new int[8];
+        int i;
+        for(i = 0; i < 8; i = i+1)
+            sample[i] = i;
+        for(i = 0; i < 8; i = i+1)
+            Console.Write( sample[i]);
+    }
+}
